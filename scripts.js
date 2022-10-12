c3e0e72557c37909d95595e5ffb860e360e40488
Valeria <vmoraleshuaman@gmail.com>
2022-10-12 10:41:47 +1030

    why choose us

--- scripts.js
+++ scripts.js
@@ -71,35 +71,6 @@ $('.slider-container').slick({
 });
 
 
-// Final Banner Slider
-
-let slideIndex = 0;
-showSlides(5);
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  if (slides[slideIndex - 1]) {
-    slides[slideIndex - 1].style.display = "block";
-  }
-  if (dots[slideIndex - 1]) {
-    dots[slideIndex - 1].className += " active";
-  }
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
-}
-
-
   // Accordion FAQ
 
   var allPanels = $('.accordion > dd').hide();
